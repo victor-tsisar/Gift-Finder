46b5af3a740ad4231e9ecd8c3a6fdd5c286e9188
Victor Tsisar <tsisar.victor@gmail.com>
2021-02-03 00:31:39 +0200

    modal window's script

--- js/main.js
+++ js/main.js
@@ -1,18 +1,77 @@
 "use strict";
 
+const menu = document.querySelector(".modal");
+const menuBtn = document.querySelectorAll(".main-btn");
+const menuClose = document.querySelector(".modal__close");
+const secondScreenBtn = document.querySelector('.second-screen__btn');
+const contactHow = document.querySelector('.contact__how');
+const modalForm = document.querySelector('.modal__form');
+const personName = document.querySelector('.person-name');
+const personPhone = document.querySelector('.person-phone');
+const personMessage = document.querySelector('.person-message');
+const personSend = document.querySelector('.person-send');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
+function allertWindow() {
+    swal({
+        text: "This is a demo version!",
+        icon: "success",
+        button: "OK!",
+    });
+}
+
+function toggleModal() {
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    modalForm.reset();
+    personMessage.style.height = '100px';
+}
+
+personMessage.addEventListener('keyup', function () {
+    if (this.scrollTop > 0) {
+        this.style.height = this.scrollHeight + "px";
+    } 
+});
+
+secondScreenBtn.addEventListener('click', allertWindow);
+contactHow.addEventListener('click', allertWindow);
+for (let i = 0; i < menuBtn.length; i++) {
+    menuBtn[i].addEventListener('click', () => {
+        toggleModal();
+        personName.focus();
+    } 
+    );
+}
+menuClose.addEventListener('click', () => {
+    toggleModal();
+    modalForm.reset();
+});
+overlay.addEventListener('click', toggleModal);
+personSend.addEventListener('click', (event) => {
+    if (personName.value && personPhone.value && personMessage.value) {
+        event.preventDefault();
+        setTimeout(allertWindow, 200);
+        toggleModal();
+    } else {
+        alert(`
+        Enter your information about the desired gifts and we will call you.
+
+        Or you call us)
+        `);
+    }
+});
+
+// Закриття модального вікна по кнопці ESC
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        toggleModal();
+    };
+});
+
 $(document).ready(function () {
-    var menu = $(".modal");
-    var menuBtn = $(".main-btn");
-    var menuClose = $(".modal__close");
-
-    menuBtn.on('click', function () {
-        menu.addClass("active");
-    });
-
-    menuClose.on('click', function () {
-        menu.removeClass("active");
-    });
-
     $('.blog__items').slick({
         arrows: false,
         infinite: true,
@@ -23,20 +82,20 @@ $(document).ready(function () {
         centerMode: true,
         adaptiveHeight: true,
         responsive: [{
-                breakpoint: 999,
-                settings: {
-                    slidesToShow: 1,
-                    centerMode: false,
-                    adaptiveHeight: false,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    autoplay: false,
-                }
+            breakpoint: 999,
+            settings: {
+                slidesToShow: 1,
+                centerMode: false,
+                adaptiveHeight: false,
             }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                autoplay: false,
+            }
+        }
         ]
     });
 
